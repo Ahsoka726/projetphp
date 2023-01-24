e16b36e5da3d7db76d05e5f287b9f8845358b13e
HenryRemi <remihenry5@live.fr>
2023-01-24 16:48:29 +0100

    Début intégration HTML CSS

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -2,16 +2,25 @@
 
 include(__DIR__.'/../views/templates/header.php');
 
-$urlFeed = "https://www.eveonline.com/rss/news";
+$rssGameLink = "https://www.eveonline.com/rss/news";
+$rssSportLink = "https://rmcsport.bfmtv.com/rss/football/";
+$rssNewsLink = "https://www.lemonde.fr/rss/une.xml";
+$rssTechLink = "https://www.01net.com/actualites/feed/";
+$rssMusicLink = "https://www.hits1radio.com/category/music/feed/";
 
-$nbrArticles = 6;
+$nbrArticles = 1;
 
-$rss = simplexml_load_file($urlFeed);
-$rssSport = simplexml_load_file($urlFeed);
-$rssTech = simplexml_load_file($urlFeed);
+$rssGameObject = simplexml_load_file($rssGameLink);
+$rssSportObject = simplexml_load_file($rssSportLink);
+$rssNewsObject = simplexml_load_file($rssNewsLink);
+$rssTechObject = simplexml_load_file($rssTechLink);
+$rssMusicObject = simplexml_load_file($rssMusicLink);
 
-$tableauInfos = array();
-
+$rssGameArray = array();
+$rssSportArray = array();
+$rssNewsArray = array();
+$rssTechArray = array();
+$rssMusicArray = array();
 
 /**
  * @param object $rss Je récupére les liens des RSS que l'utilisateur à choisis
@@ -25,19 +34,15 @@ function rssFeed(object $rss,int $nbrArticles,array $array):array{
     for ($i=0; $i < $nbrArticles ; $i++) { 
         $array[$i]['title'] = $rss->channel->item[$i]->title;
         $array[$i]['link'] = $rss->channel->item[$i]->link;
-        $array[$i]['desc'] = $rss->channel->item[$i]->description;
+        $array[$i]['desc'] = substr($rss->channel->item[$i]->description,0,250);
     }
     // var_dump($array);
     return $array;
 }
 
-$tableauInfos = rssFeed($rss,$nbrArticles,$tableauInfos);
-var_dump($tableauInfos);
-
-
-
-
-
+$rssGameArray = rssFeed($rssGameObject,$nbrArticles,$rssGameArray);
+$rssSportArray = rssFeed($rssSportObject,$nbrArticles,$rssSportArray);
+$rssNewsArray = rssFeed($rssNewsObject,$nbrArticles,$rssNewsArray);
 
 include(__DIR__ . '/../views/home.php');
 
--- views/home.php
+++ views/home.php
@@ -1,20 +1,114 @@
-    
     <div class="row justify-content-center align-items-center g-2">
-        <?php
-            foreach ($tableauInfos as $key => $value) {
-                foreach ($value as $key => $value) {
-                    if($key == 'title'){
-                        echo '<h2>'.$value.'</h2>';
-                    }elseif ($key == 'link') {
-                        echo '<a href='.$value.'>'.$value.'</a>';
-                    }elseif ($key == 'desc') {
-                        echo '<p>'.$value.'</p>';
-                    }
+            <div class="card">
+                <?php foreach ($rssGameArray as $key => $value) {    ?>
+                    <img class="card-img-top" src="holder.js/100x180/" alt="Card image cap">
+                    <?php foreach ($value as $key => $value) {       ?>
 
-                }
-            }
-        
-        ?>
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <span class="col-6"><?php if ($key == 'title') {
+                                                        echo  $value  ?></span>
+                            </h5>
+                            <p class="card-text">
+                            <?php  } elseif ($key == 'desc') {
+                                                        echo '<p>' . $value . '</p>';; ?>
+                            </p>
+                            <a href="#" tabindex="-1" class="btn btn-primarycol-6">
+                    <?php   } elseif ($key == 'link') {
+                                                        echo '<a href=' . $value . '>' . $value . '</a>';
+                                                    }
+                                                    break;
+
+                                                }
+                                                break;
+                                            }
+                    ?>
+                            </a>
+                        </div>
+            </div>
+            <div class="card">
+                <?php foreach ($rssSportArray  as $key => $value) {    ?>
+                    <img class="card-img-top" src="holder.js/100x180/" alt="Card image cap">
+                    <?php foreach ($value as $key => $value) {       ?>
+
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <span class="col-6"><?php if ($key == 'title') {
+                                                        echo  $value  ?></span>
+                            </h5>
+                            <p class="card-text">
+                            <?php  } elseif ($key == 'desc') {
+                                                        echo '<p>' . $value . '</p>';; ?>
+                            </p>
+                            <a href="#" tabindex="-1" class="btn btn-primarycol-6">
+                    <?php   } elseif ($key == 'link') {
+                                                        echo '<a href=' . $value . '>' . $value . '</a>';
+                                                    }
+                                                    break;
+
+                                                }
+                                                break;
+
+                                            }
+                    ?>
+                            </a>
+                        </div>
+            </div>
+            <div class="card">
+                <?php foreach ($rssNewsArray  as $key => $value) {    ?>
+                    <img class="card-img-top" src="holder.js/100x180/" alt="Card image cap">
+                    <?php foreach ($value as $key => $value) {       ?>
+
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <span class="col-6"><?php if ($key == 'title') {
+                                                        echo  $value  ?></span>
+                            </h5>
+                            <p class="card-text">
+                            <?php  } elseif ($key == 'desc') {
+                                                        echo '<p>' . $value . '</p>';; ?>
+                            </p>
+                            <a href="#" tabindex="-1" class="btn btn-primarycol-6">
+                    <?php   } elseif ($key == 'link') {
+                                                        echo '<a href=' . $value . '>' . $value . '</a>';
+                                                    }
+
+                                                }
+                                                break;
+
+                                            }
+                    ?>
+                            </a>
+                        </div>
+            </div>
+        <!-- <div class="card-group">
+            <div class="card">
+                <img class="card-img-top" src="holder.js/100x180/" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="holder.js/100x180/" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+        </div> -->
+
+
+        <!-- <div class="col-3">
+            <?php
+            // foreach ($rssGameArray as $key => $value) {
+            //     foreach ($value as $key => $value) {
+
+
+
+
+            ?> -->
+
+
     </div>
-   
-          
+    </div>
